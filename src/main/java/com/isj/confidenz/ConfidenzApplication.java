c0c3f65805410a77fcf7221d41be1812c952e56a
MaximeAto <maxime.nkeng@institutsaintjean.org>
2023-06-13 22:48:27 +0100

    config du endpoint de la racine

--- src/main/java/com/isj/confidenz/ConfidenzApplication.java
+++ src/main/java/com/isj/confidenz/ConfidenzApplication.java
@@ -3,6 +3,10 @@ package com.isj.confidenz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConfidenzApplication {
@@ -10,5 +14,13 @@ public class ConfidenzApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfidenzApplication.class, args);
 	}
+}
 
+@Controller
+class HelloApp {
+	@GetMapping("/")
+	@ResponseBody
+	public String hello() {
+		return "good job";
+	}
 }
